aa7d88988897237810e1131757f1c9fe6ce9e028
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2024-03-08 16:59:03 -0300

    write a response

--- app/server.go
+++ app/server.go
@@ -14,9 +14,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	n, err := conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error sending data: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("sent %d bytes", n)
 }
